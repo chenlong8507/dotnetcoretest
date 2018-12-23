f4b8089f5e1d4f7395534234c56894e75d46afd5
CHEN Long L <long.l.chen@hexagon.com>
2018-12-23 11:24:31 +0800

    181223test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloDotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            System.Console.WriteLine("2");
+            Console.ReadLine();
+
+            Person person = new Person(){
+                Id=1,
+                Name="chenlong"
+            };
+            System.Console.WriteLine(person.Name);
+            Console.ReadLine();
+            
+        }
+    }
+}
